c5cc7ae4b7424d681f9276e4fb2a83df9a3c6d57
rnydv3004 <yaryan3087@gmail.com>
2024-01-20 13:34:14 +0530

    serverjs

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Define custom routes here, if needed
+  server.get("/custom-route", (req, res) => {
+    return app.render(req, res, "/custom-page", req.query);
+  });
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  const port = process.env.PORT || 3000;
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
