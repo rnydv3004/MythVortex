50026f2c8ef424fdb9ded9119df72627499c5809
rnydv3004 <yaryan3087@gmail.com>
2024-04-14 11:13:11 +0530

    fix chnages

--- app/(site)/blog/[postId]/page.tsx
+++ app/(site)/blog/[postId]/page.tsx
@@ -14,35 +14,35 @@ import Link from "next/link";
 //   // other metadata
 // };
 
-import type { Metadata, ResolvingMetadata } from "next";
+// import type { Metadata, ResolvingMetadata } from "next";
 
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+// type Props = {
+//   params: { id: string };
+//   searchParams: { [key: string]: string | string[] | undefined };
+// };
 
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
-  // read route params
-  const id = params.id;
+// export async function generateMetadata(
+//   { params, searchParams }: Props,
+//   parent: ResolvingMetadata,
+// ): Promise<Metadata> {
+//   // read route params
+//   const id = params.id;
 
-  // fetch datace
-  const product = await fetch(`https://mythvortex.com/blog/${id}`).then((res) =>
-    res.json(),
-  );
-  console.log(product);
-  // optionally access and extend (rather than replace) parent metadata
-  const previousImages = (await parent).openGraph?.images || [];
+//   // fetch datace
+//   const product = await fetch(`https://mythvortex.com/blog/${id}`).then((res) =>
+//     res.json(),
+//   );
+//   console.log(product);
+//   // optionally access and extend (rather than replace) parent metadata
+//   const previousImages = (await parent).openGraph?.images || [];
 
-  return {
-    title: product.title,
-    openGraph: {
-      images: ["/some-specific-page-image.jpg", ...previousImages],
-    },
-  };
-}
+//   return {
+//     title: product.title,
+//     openGraph: {
+//       images: ["/some-specific-page-image.jpg", ...previousImages],
+//     },
+//   };
+// }
 
 const SingleBlogPage = ({ params }) => {
   const [blogData, setBlogData] = useState<any>();
