602fb6cdffd0f8f1de943834f03c410592756a7c
rnydv3004 <yaryan3087@gmail.com>
2024-06-14 13:19:16 +0530

    update

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -155,7 +155,7 @@ const Hero = () => {
                       className="animate-floatYR  overflow-hidden rounded-sm"
                       href={"/appointment"}
                     >
-                      <div className="flex flex-col items-center justify-center bg-gradient-to-tr from-slate-700 via-slate-800 to-transparent text-white rounded-md py-2 border-2 dark:border-slate-600">
+                      <div className="flex flex-col items-center justify-center dark:bg-gradient-to-tr bg-white dark:bg-transparent dark:from-slate-700 dark:via-slate-800 dark:to-transparent text-primary dark:text-white rounded-md py-2 border-2 dark:border-slate-600">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -170,8 +170,8 @@ const Hero = () => {
                             d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
                           />
                         </svg>
-                        <h4 className="text-center uppercase text-white font-semibold text-sm">
-                          Book
+                        <h4 className="text-center uppercase dark:text-white font-semibold text-sm">
+                          Free
                           Consultation
                         </h4>
                       </div>
